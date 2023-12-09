205cbbeac9197d5998ad64f2d2bad7698086237a
NemoNJ <152589734+NemoNJ@users.noreply.github.com>
2023-12-09 13:45:18 +0700

    Add files via upload

---
+++ lab7_1.cpp
@@ -0,0 +1,13 @@
+//lab7_1.cpp
+#include <iostream>
+using namespace std;
+int main() {
+    int a  =  20; 
+    double b = 30;
+    int c = 4; 
+    double output;
+
+    output = a - b/c;
+    cout << "output: " << output ;
+    return 0;
+}
